24162e6043c517c8cb61ba02403b8fd9b965c268
Gourav Jain <gourav.jain@bold.com>
2022-11-24 01:53:23 +0530

    Changes-11

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 /* eslint-disable prettier/prettier */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 5000;
+  const port = process.env.PORT || 3000;
   await app.listen(port);
 }
 bootstrap();
